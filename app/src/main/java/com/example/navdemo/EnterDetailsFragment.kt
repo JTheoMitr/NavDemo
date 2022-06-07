912d22bacbeea04bed3a401e106da87673d5df6a
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-06-06 23:03:39 -0500

    now passing user info from EnterDetailsFragment.kt

--- app/src/main/java/com/example/navdemo/EnterDetailsFragment.kt
+++ app/src/main/java/com/example/navdemo/EnterDetailsFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 
 
@@ -24,9 +27,31 @@ class EnterDetailsFragment : Fragment() {
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_enter_details, container, false)
 
+        val etName = rootView.findViewById<EditText>(R.id.et_first_name)
+        val etMobileNumber = rootView.findViewById<EditText>(R.id.et_mobile)
+
         val btnVerifyDetails = rootView.findViewById<Button>(R.id.btn_verify_details)
         btnVerifyDetails.setOnClickListener {
-            findNavController().navigate(R.id.action_enterDetailsFragment_to_verifyDetailsFragment)
+
+            val firstName = etName.text.toString()
+            val mobile = etMobileNumber.text.toString()
+
+            when {
+                firstName.isEmpty() -> {
+                    Toast.makeText(activity, "Enter Name", Toast.LENGTH_LONG).show()
+                }
+                mobile.isEmpty() -> {
+                    Toast.makeText(activity, "Enter Mobile Number", Toast.LENGTH_LONG).show()
+                }
+                else -> {
+                    val bundle = bundleOf(
+                        "name" to firstName,
+                        "mobile" to mobile.toLong()
+                    )
+
+                    findNavController().navigate(R.id.action_enterDetailsFragment_to_verifyDetailsFragment, bundle)
+                }
+            }
         }
         return rootView
     }
