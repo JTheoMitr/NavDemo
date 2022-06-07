c94c59bdbfac6a7532eea97cf6c9b0bf9af2018b
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-06-07 12:12:49 -0500

    updated Verify Details Fragment with viewBinding

--- app/src/main/java/com/example/navdemo/VerifyDetailsFragment.kt
+++ app/src/main/java/com/example/navdemo/VerifyDetailsFragment.kt
@@ -6,31 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.example.navdemo.databinding.FragmentVerifyDetailsBinding
 
 class VerifyDetailsFragment : Fragment() {
 
+    private var _binding: FragmentVerifyDetailsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        _binding = FragmentVerifyDetailsBinding.inflate(inflater, container, false)
+
         val name = arguments?.getString("name")
         val mobileNumber = arguments?.getLong("mobile")
 
-        val rootView = inflater.inflate(R.layout.fragment_verify_details, container, false)
-        val tvName = rootView.findViewById<TextView>(R.id.tv_name)
-        val tvMobileNumber = rootView.findViewById<TextView>(R.id.tv_mobile_number)
+//        val rootView = inflater.inflate(R.layout.fragment_verify_details, container, false)
+//        val tvName = rootView.findViewById<TextView>(R.id.tv_name)
+//        val tvMobileNumber = rootView.findViewById<TextView>(R.id.tv_mobile_number)
 
-        tvName.text = name
-        tvMobileNumber.text = mobileNumber.toString()
+        binding.tvName.text = name
+        binding.tvMobileNumber.text = mobileNumber.toString()
 
         // Inflate the layout for this fragment
-        return rootView
+        return binding.root
     }
 
 }
