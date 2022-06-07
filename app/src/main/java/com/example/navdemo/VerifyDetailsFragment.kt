703acd66bd2252a3f2ba8c31a3ea1bd77481f9a8
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-06-06 23:22:27 -0500

    Now receiving data in verify details fragment

--- app/src/main/java/com/example/navdemo/VerifyDetailsFragment.kt
+++ app/src/main/java/com/example/navdemo/VerifyDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class VerifyDetailsFragment : Fragment() {
 
@@ -17,8 +18,19 @@ class VerifyDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val name = arguments?.getString("name")
+        val mobileNumber = arguments?.getLong("mobile")
+
+        val rootView = inflater.inflate(R.layout.fragment_verify_details, container, false)
+        val tvName = rootView.findViewById<TextView>(R.id.tv_name)
+        val tvMobileNumber = rootView.findViewById<TextView>(R.id.tv_mobile_number)
+
+        tvName.text = name
+        tvMobileNumber.text = mobileNumber.toString()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_verify_details, container, false)
+        return rootView
     }
 
 }
